cf3a19c33b4c5f2576a9a6ad44cc8a9481861809
Yoshinori Isogai <isogai.shiraji@gmail.com>
2016-04-14 18:50:31 +0900

    Create after_success.sh

---
+++ after_success.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 
+# Travis ci script for releasing gradle project
+# Adapted from http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+# Change this
+REPO=""
+
+# Change if necessary
+USER="shiraji"
+JDK="oraclejdk7"
+BRANCH="master"
+
+# Not handling errors
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$USER/$REPO" ]; then
+  # Check repo
+  echo "TRAVIS_REPO_SLUG: '$TRAVIS_REPO_SLUG' USER/REPO: '$USER/$REPO'"
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  # Check JDK 
+  echo "TRAVIS_JDK_VERSION: '$TRAVIS_JDK_VERSION' JDK: '$JDK"
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  # Check how to run this script
+  echo "It's pull request!"
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  # Check branch
+  echo "TRAVIS_BRANCH: '$TRAVIS_BRANCH' BRANCH: '$BRANCH'"
+else
+  echo "Start releasing..."
+  ./release.sh
+fi
