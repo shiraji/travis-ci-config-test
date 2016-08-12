32211c1752d015c66f7b8c3402fc387a700a4409
shiraji <isogai.shiraji@gmail.com>
2016-08-12 13:34:57 +0900

    Make release file

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -28,7 +28,7 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "TRAVIS_BRANCH: '$TRAVIS_BRANCH' BRANCH: '$BRANCH'"
 else
   echo "Start releasing..."
-  touch foo.txt
+  rm .travis/release
   git config user.name "Travis CI"
   git config user.email "isogai.shiraji@gmail.com"
   git add .
